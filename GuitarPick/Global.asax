87cc1e07256c7ed83952ef9f355a5bdd43e153b4
beaud <beaud@BEAUDY-LAPTOP>
2017-06-02 09:45:52 -0700

    Add project files.

---
+++ GuitarPick/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuitarPick.MvcApplication" Language="C#" %>
---
+++ GuitarPick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GuitarPick.Startup))]
+namespace GuitarPick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
